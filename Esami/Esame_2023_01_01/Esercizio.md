d93e11f80d35ecac31e647c646f3b8801cd32490
Canghiari04 <matteo.canghiari@gmail.com>
2024-05-26 11:19:17 +0200

    completato Esame 01/01/2023

---
+++ Esami/Esame_2023_01_01/Esercizio.md
@@ -0,0 +1,28 @@
+# Pre-esame
+
+### A. Il metodo HEAD
+
+### Risposta
+Il metodo __HEAD__ appartiene all'insieme dei verbi del protocollo di trasporto __HTTP__. Rappresenta un metodo sia __sicuro__ che __idempotente__, poichè non provoca alcun __cambiamento__ allo stato interno del __Server__ contenitore delle risorse e, se applicato individualmente oppure molteplici volte, adegua sempre lo stesso __effetto__. 
+
+### B. DOM
+
+### Risposta
+L'acronimo __DOM__ consiste in __Document Object Model__, ossia rappresenta un __API__ della pagina HTML visualizzata su browser. Lo scopo principale di uno strumento simile è dettato dall'estrema semplicità di manipolazione, gestione e controllo della risorsa esplicitamente interpretata.
+
+### C. HTML
+
+### Risposta
+__HTML__, __Hyper Text Markup Language__, è un __linguaggio di markup__, quindi definito per dichiarare l'interpretazione esplicita di una risorsa. Esso deriva dal __meta-linguaggio__ __SGML__, il quale ne definisce le regole e le grammatiche di utilizzo.
+
+### D. Quale tra i seguenti selettori CSS seleziona tutti i paragrafi contenuti in una lista ordinata?
+
+### Risposta
+ol, p 
+
+
+### E. Spiegare il concetto di prototype in Javascript
+
+### Risposta 
+Il __Prototype__ è una caratteristica comune a tutti gli oggetti definiti in __Javascript__, pertanto definisce i cosiddetti __prototyped language__, ossia i linguaggi che pongono il prototipo come elemento principale; a discapito dei linguaggi __class based__, i quali definiscono la classe come principale concetto. \
+Il Prototype è una proprietà comune a tutti gli oggetti, manipolabile, gestibile e modificabile in qualsiasi momento anche durante run-time. Una propria particolarità è dettata dalla possibilità di definire, mediante il prototipo, nuove funzionalità oppure attributi; pertanto, qualora siano definiti cloni di un oggetto precedentemente alla modifica del prototipo, si osserva come la variazione sia __rilevante__ anche per istanze adeguate previamente.
--- Esami/Esame_2023_01_01/Esercizio2.js
+++ Esami/Esame_2023_01_01/Esercizio2.js
@@ -1,29 +1,53 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("a-africa").addEventListener("click", function() {
-        getCities(document.getElementById("list-cities"), document.getElementById("a-africa").innerHTML)
+        changeCSS(document.getElementById("a-africa"));
+        getResources(document.getElementById("a-africa").name);
     })
 
-    document.getElementById("a-europe").addEventListener("click", function() {
-        getCities(document.getElementById("list-cities"), document.getElementById("a-europe").innerHTML)
+    document.getElementById("a-europa").addEventListener("click", function() {
+        changeCSS(document.getElementById("a-europa"));
+        getResources(document.getElementById("a-europa").name);
     })
 
-    document.getElementById("a-australia").addEventListener("click", function() {
-        getCities(document.getElementById("list-cities"), document.getElementById("a-australia").innerHTML)
+    document.getElementById("a-oceania").addEventListener("click", function() {
+        changeCSS(document.getElementById("a-oceania"));
+        getResources(document.getElementById("a-oceania").name);
     })
 
     document.getElementById("a-asia").addEventListener("click", function() {
-        getCities(document.getElementById("list-cities"), document.getElementById("a-asia").innerHTML)
+        changeCSS(document.getElementById("a-asia"));
+        getResources(document.getElementById("a-asia").name);
     })
 })
 
-function getCities(listCities, typeContinent) {
-    /*
-     * ATTENZIONE --> Il servizio .php a cui connettersi è disabilitato
-     * Dato che il servizio è disabilitato, definisco qui di seguito come avrei proceduto.
-     * 
-     * Acquisita la lista delle città "listCities" avrei immesso gli input al suo interno
-     * mediante un ciclo for. La tipologia di città inserite è strettamente dipendente dalla
-     * tipologia di continente; in base al risultato della funzione "contains(typeContinent)"
-     * la lista sarebbe stata costruita dinamicamente.   
-     */
+function changeCSS(country) {
+    var _class = document.createAttribute("class");
+    _class.value = "selected"; 
+
+    country.setAttributeNode(_class);
+}
+
+function getResources(country) {
+    document.getElementById("header-country").innerHTML = `Le fantastiche città da visitare in ${country}:`;
+
+    var request = new XMLHttpRequest();
+    request.open("GET", "", true);
+
+    request.onload = () => {
+        (request.status === 200) ? (addList(country, JSON.parse(request.response), document.getElementById("ol-cities"))) : (console.error("Errore durante la richiesta"))
+    }
+
+    request.send();
+}
+
+function addList(country, items, list) {
+    for(let i = 0; i <= items.length - 1; i++) {
+        if(items[i].id === country) {
+            /* add di tutte le città all'interno della lista, a cui si applica la proprietà onclick, per visualizzare correttamente il contenuto del city header */
+        }
+    }
+}
+
+function changeCity() {
+    /* si dovrebbe agire sul city header, rispetto al radio button selezionato */
 }
