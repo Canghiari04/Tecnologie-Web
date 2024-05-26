8241b28aee9a00b2ca4a3fc705693f807e3f3cd4
Canghiari04 <matteo.canghiari@gmail.com>
2024-05-26 21:51:20 +0200

    completato Esame 02/01/2021

--- Esami/Esame_2021_01_02/Esercizio3.js
+++ Esami/Esame_2021_01_02/Esercizio3.js
@@ -1,138 +1,156 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("menu-terra").disabled = true;
-    document.getElementById("menu-mare").disabled = true;
-    document.getElementById("menu-stagione").disabled = true;
+    /* caricato il contenuto dell'interpretazione esplicita del documento, sono disabilitate le select per la scelta dei menu */
+    disableSelect();
 
-    document.getElementById("numero-persone").addEventListener("click", function() {
-        document.getElementById("menu-mare").disabled = false;
-        document.getElementById("menu-terra").disabled = false;
-        document.getElementById("menu-stagione").disabled = false;
-
-        removeOptions()
-        addOptions(document.getElementById("numero-persone").value);
-    });
+    document.getElementById("select-people").addEventListener("click", function() {
+        /* clear dei fieldset dei menu */
+        document.getElementById("div-menu-mare").innerHTML = "";   
+        document.getElementById("div-menu-terra").innerHTML = "";   
+        document.getElementById("fieldset-menu-mare").style.visibility = "hidden";
+        document.getElementById("fieldset-menu-terra").style.visibility = "hidden";
+        
+        activeSelect();
+        changeNumberMenus(document.getElementById("select-people").value);
+    }) 
 
     document.getElementById("menu-mare").addEventListener("click", function() {
+        /* numero di scelte selezionate */
         var value = document.getElementById("menu-mare").value;
 
-        /* numero di options relative alla select circoscritta */
-        var length = document.getElementById("menu-mare").options.length - 1;
+        /* numero di scelte di partenza */
+        var length = document.getElementById("menu-mare").options.length;
 
-        /* remove delle options dalla select */
-        removeOptions();
+        /* verificando la differenza tra le due variabili, è possibile accertarsi, in tempo reale, il numero effettivo di scelte rimanenti */
+        var difference = length - value;
 
-        /* aggiorna il contenuto del menu */
-        updateMenu("mare", document.getElementById("menu"));
+        getMenus("mare");
 
-        /* condizione che si accerta che siano presenti ancora Scelte per i menu */
-        ((length - value) == 0) ? disabledSelect() : addOptions(length - value);
-    });
+        (difference > 0) ? (changeNumberMenus(length - value)) : disableSelect(); 
+    })
 
     document.getElementById("menu-terra").addEventListener("click", function() {
         var value = document.getElementById("menu-terra").value;
-        var length = document.getElementById("menu-terra").options.length - 1;
+        var length = document.getElementById("menu-terra").options.length;
 
-        removeOptions();
-        updateMenu("terra", document.getElementById("menu"));
-        ((length - value) == 0) ? disabledSelect() : addOptions(length - value);
-    });
+        var difference = length - value;
+
+        getMenus("terra");
+
+        (difference > 0) ? (changeNumberMenus(length - value)) : disableSelect(); 
+    })
 
     document.getElementById("menu-stagione").addEventListener("click", function() {
         var value = document.getElementById("menu-stagione").value;
-        var length = document.getElementById("menu-stagione").options.length - 1;
+        var length = document.getElementById("menu-stagione").options.length;
 
-        removeOptions();
-        ((length - value) == 0) ? disabledSelect() : addOptions(length - value);
-    });
-});
+        var difference = length - value;
 
-/* la rimozione di options dalla select richiede che sia eliminate in maniera decrescente, ossia da value 3 --> 2 --> 1 --> 0 */
-function removeOptions() {
-    var length = document.getElementById("menu-stagione").options.length - 1;
+        getMenus("stagione");
 
-    for(let i = length; i >= 0; i--) {
-        document.getElementById("menu-mare").remove(i);
-        document.getElementById("menu-terra").remove(i);
-        document.getElementById("menu-stagione").remove(i);
-    }
-}
+        (difference > 0) ? (changeNumberMenus(length - value)) : disableSelect(); 
+    })
+}) 
 
-function updateMenu(type, divMenu) {
-    var request = new XMLHttpRequest();
-    request.open("GET", "http://diiorio.nws.cs.unibo.it/twe/api/menu/menu.php", true);
-
-    request.onload = () => {
-        if(request.status == '200') {
-            var tokens = JSON.parse(request.response);
-
-            for(let index in tokens) {
-                getMenu(type, item = tokens[index], divMenu);
-            }
-
-        } else if(request.status == '400') {
-            console.log("Errore durante la richiesta.");
-        }
-    }
-
-    request.send();
-}
-
-function getMenu(type, item, divMenu) {
-    if(item.id.includes(type)) {
-        var fieldset = document.createElement("fieldset");
-
-        var legend = document.createElement("legend");
-        legend.innerHTML = item.nome;
-
-        var paragraphAntipasto = document.createElement('p');
-        paragraphAntipasto.innerHTML = item.antipasto.nome + " --> " + item.antipasto.descrizione;
-                    
-        var paragraphPrimo = document.createElement('p');
-        paragraphPrimo.innerHTML = item.primo.nome + " --> " + item.antipasto.descrizione;
-
-        var paragraphSecondo = document.createElement('p');
-        paragraphSecondo.innerHTML = item.secondo.nome + " --> " + item.antipasto.descrizione;
-
-        fieldset.appendChild(legend);
-        fieldset.appendChild(paragraphAntipasto);
-        fieldset.appendChild(paragraphPrimo);
-        fieldset.appendChild(paragraphSecondo);
-        divMenu.appendChild(fieldset);
-    }
-}
-
-function disabledSelect() {
+function disableSelect() { 
     document.getElementById("menu-mare").disabled = true;
     document.getElementById("menu-terra").disabled = true;
     document.getElementById("menu-stagione").disabled = true;
 }
 
-function addOptions(numPeople) {
+function changeNumberMenus(numberPeople) { 
     var selectMenuMare = document.getElementById("menu-mare");
     var selectMenuTerra = document.getElementById("menu-terra");
     var selectMenuStagione = document.getElementById("menu-stagione");
 
-    for(let i = numPeople; i >= 0; i--) {
+    clear(selectMenuMare, selectMenuTerra, selectMenuStagione);
+
+    for(let i = 0; i <= numberPeople - 1; i++) {
         var option = document.createElement("option");
-        option.value = i;
-        option.innerHTML = i;
+        option.innerHTML = parseInt(i + 1);
+
+        var value = document.createAttribute("value");
+        value.value = parseInt(i + 1);
+
+        option.setAttributeNode(value);
 
         selectMenuMare.appendChild(option);
     }
 
-    for(let i = numPeople; i >= 0; i--) {
+    for(let i = 0; i <= numberPeople - 1; i++) {
         var option = document.createElement("option");
-        option.value = i;
-        option.innerHTML = i;
+        option.innerHTML = parseInt(i + 1);
+
+        var value = document.createAttribute("value");
+        value.value = parseInt(i + 1);
+
+        option.setAttributeNode(value);
 
         selectMenuTerra.appendChild(option);
     }
 
-    for(let i = numPeople; i >= 0; i--) {
+    for(let i = 0; i <= numberPeople - 1; i++) {
         var option = document.createElement("option");
-        option.value = i;
-        option.innerHTML = i;
+        option.innerHTML = parseInt(i + 1);
+
+        var value = document.createAttribute("value");
+        value.value = parseInt(i + 1);
+
+        option.setAttributeNode(value);
 
         selectMenuStagione.appendChild(option);
     }
+}
+
+function clear(selectMenuMare, selectMenuTerra, selectMenuStagione) {
+    selectMenuMare.innerHTML = "";
+    selectMenuTerra.innerHTML = "";
+    selectMenuStagione.innerHTML = "";
+}
+
+function activeSelect() {
+    document.getElementById("menu-mare").disabled = false;
+    document.getElementById("menu-terra").disabled = false;
+    document.getElementById("menu-stagione").disabled = false;
+}
+
+/* visualizzazione grafica del menu selezionato */
+function getMenus(type) {
+    var request = new XMLHttpRequest();
+    request.open("GET", "http://diiorio.nws.cs.unibo.it/twe/api/menu/menu.php", true);
+    
+    request.onload = () => {
+        if(request.status === 200) {
+            var items = JSON.parse(request.response);
+
+            for(let i = 0; i <= items.length - 1; i++) {
+                var fieldset = document.getElementById(`fieldset-menu-${type}`);
+                var legend = document.getElementById(`legend-menu-${type}`);                
+                var div = document.getElementById(`div-menu-${type}`);   
+                div.innerHTML = "";
+
+                if(items[i].id.includes(type.toLowerCase())) {
+                    legend.innerText = items[i].nome;
+
+                    var paragraphAntipasto = document.createElement("p");
+                    paragraphAntipasto.innerHTML = `Antipasto: ${items[i].antipasto.nome}`;
+
+                    var paragraphPrimo = document.createElement("p");
+                    paragraphPrimo.innerHTML = `Primo: ${items[i].primo.nome}`;
+
+                    var paragraphSecondo = document.createElement("p");
+                    paragraphSecondo.innerHTML = `Secondo: ${items[i].secondo.nome}`;
+
+                    div.appendChild(paragraphAntipasto);
+                    div.appendChild(paragraphPrimo);
+                    div.appendChild(paragraphSecondo);
+
+                    fieldset.style.visibility = "visible";
+                }
+            }
+        } else {
+            console.error("Errore durante la richiesta");
+        } 
+    }
+    
+    request.send();
 }
