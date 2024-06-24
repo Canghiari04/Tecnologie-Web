ca996825bffc24a61478a4858797f9ecee5f602c
Canghiari04 <matteo.canghiari@gmail.com>
2024-06-24 18:37:24 +0200

    completata Simulazione Lecture 6

--- Esercizi_Lecture/Esercizi_Lect6/Esercizio2.js
+++ Esercizi_Lecture/Esercizi_Lect6/Esercizio2.js
@@ -1,23 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("button-submit").addEventListener("click", function() {
-        var span = document.getElementById("span-timer");
-        var time = document.getElementById("input-time").value;
-
-        span.style.visibility = "visible";
-        document.getElementById("button-submit").disabled = true;
-
-        /* <--> */
-        var timer = setInterval(function() {
-            span.innerHTML = time;
-
-            if(time > 0) {
-                time--;
-            } else {
-                span.innerHTML = "Fatto!"
-                clearInterval(timer);
-
-                document.getElementById("button-submit").disabled = false;
-            }
-        } , 1000);
-    })
-})
+	document.getElementById("button-submit").addEventListener("click", function() {
+		var time = document.getElementById("input-time").value;
+		
+		document.getElementById("span-timer").style.visibility = "visible";
+		
+		if(time == "") {
+			document.getElementById("span-timer").innerHTML = "INSERISCI PRIMA I SECONDI!"
+		} else {
+			document.getElementById("button-submit").disabled = true;
+			document.getElementById("span-timer").innerHTML = time;
+		
+			var timer = setInterval(function() {
+				time = time - 1;
+				(time > 0) ? (document.getElementById("span-timer").innerHTML = time) : (document.getElementById("span-timer").innerHTML = "Fatto!")
+			}, 1000);
+			
+			document.getElementById("button-submit").disabled = false;
+			document.getElementById("span-timer").innerHTML = "";
+		}
+	});
+});
