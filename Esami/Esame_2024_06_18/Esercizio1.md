7e5b1233f8be1d18eca6a2f80dba43b483169aac
Canghiari04 <matteo.canghiari@gmail.com>
2024-06-24 17:11:19 +0200

    completato Esame 18/06/2024

---
+++ Esami/Esame_2024_06_18/Esercizio1.md
@@ -0,0 +1,8 @@
+# Domande di Base
+
+### A - Quanti byte sono richiesti in UTF-8 per rappresentare le seguenti parole?
+* 16 Byte
+* 11 Byte
+* 6 Byte
+* 24 Byte
+* 13 Byte
--- Esami/Esame_2024_06_18/Esercizio3.js
+++ Esami/Esame_2024_06_18/Esercizio3.js
@@ -3,42 +3,55 @@ document.addEventListener("DOMContentLoaded", function() {
 		var titolo = document.getElementById("input-titolo").value;
 		var contenuto = document.getElementById("input-contenuto").value;
 		
-		var span = document.createElement("span");
-		
-		var id = document.createAttribute("id");
-		id.value = titolo;
-		
-		span.setAttributeNode(id);
-		
-		var span_titolo = document.createElement("span");
-		span_titolo.innerHTML = titolo;
-		var span_contenuto = document.createElement("span");
-		span_contenuto.innerHTML = contenuto;
-		
-		span.appendChild(span_titolo);
-		span.appendChild(span_contenuto);
-		
-		var button = document.createElement("button");
-		button.innerHTML = "Elimina";
-		
-		var id_button = document.createAttribute("id");
-		id_button.value = titolo;
-		var  f = document.createAttribute("onclick");
-		f.value = "deleteNote(id)"
-		
-		button.setAttributeNode(id_button);
-		button.setAttributeNode(f);
-		
-		span.appendChild(button);
-		
-		document.getElementById("div-notes").appendChild(span);
+		if(titolo == "") {
+			document.getElementById("input-titolo").innerHTML = "Scrivi il titolo!";
+		} 
+		else if(contenuto == "") {
+			document.getElementById("input-c").innerHTML = "Scrivi il titolo!";
+		} else {
+			var span = document.createElement("span");
+			
+			var id = document.createAttribute("id");
+			id.value = `${titolo}-${contenuto}`;
+			
+			span.setAttributeNode(id);
+			
+			var span_titolo = document.createElement("span");
+			span_titolo.innerHTML = titolo;
+			
+			var span_contenuto = document.createElement("span");
+			span_contenuto.innerHTML = contenuto;
+			
+			var button = document.createElement("button");
+			button.innerHTML = "ELIMINA";
+			
+			var id_button = document.createAttribute("id");
+			id_button.value = `${titolo}-${contenuto}`;
+			
+			var f = document.createAttribute("onclick");
+			f.value = "deleteNote(id)";
+			
+			button.setAttributeNode(id_button);
+			button.setAttributeNode(f);
+			
+			span.appendChild(span_titolo);
+			span.appendChild(span_contenuto);
+			span.appendChild(button);
+			
+			document.getElementById("div-notes").appendChild(span);
+		}
 	});	
+	
+	document.getElementById("button-reload").addEventListener("click", function() {
+		var div = document.getElementById("div-notes");
+		div.innerHTML = "";
+	});
 });
 
 function deleteNote(id) {
+	var div = document.getElementById("div-notes");
 	var spans = document.getElementsByTagName("span");
 	var span = spans.namedItem(id);
 	
-	var div_notes = document.getElementById("div-notes");
-	div_notes.removeChild(span);
+	div.removeChild(span);
 }
